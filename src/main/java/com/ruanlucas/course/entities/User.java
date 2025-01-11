9e4023887eeccf510193ad10c3570e973641e3ab
Ruan Lucas <rulucasilva13@gmail.com>
2025-01-11 15:11:47 -0300

    User update

--- src/main/java/com/ruanlucas/course/entities/User.java
+++ src/main/java/com/ruanlucas/course/entities/User.java
@@ -34,11 +34,11 @@ public class User implements Serializable{
 	public User() {
 	}
 
-	public User(Long id, String name, String emial, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.email = emial;
+		this.email = email;
 		this.phone = phone;
 		this.password = password;
 	}
@@ -59,12 +59,12 @@ public class User implements Serializable{
 		this.name = name;
 	}
 
-	public String getEmial() {
+	public String getEmail() {
 		return email;
 	}
 
-	public void setEmial(String emial) {
-		this.email = emial;
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getPhone() {
